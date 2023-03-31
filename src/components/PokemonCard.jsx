651e67a3365907376d7af9fbdc4682991f2814db
Julien Leroux <julienleroux9328@gmail.com>
2023-03-31 11:55:53 +0200

    create component

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+const PokemonCard = () => {
+  return (
+    <figure>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/94.png" alt="Ectoplasma" />
+      <figcaption>Ectoplasma</figcaption>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/149.png" alt="Ectoplasma" />
+      <p>Dracolosse</p>
+    </figure>
+  );
+}
+
+export default PokemonCard;
